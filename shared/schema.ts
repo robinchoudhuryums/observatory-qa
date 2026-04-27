c1239fb138b75a80e682e9ed45b5efa93434e81e
robinchoudhuryums <robin.choudhury@universalmedsupply.com>
2026-04-27 09:14:48 -0500

    chore(schema): export scheduled_reports tables from schema barrel

--- shared/schema.ts
+++ shared/schema.ts
@@ -4,3 +4,4 @@ export * from "./schema/calls";
 export * from "./schema/billing";
 export * from "./schema/features";
 export * from "./schema/snapshots";
+export * from "./schema/scheduled-reports";
